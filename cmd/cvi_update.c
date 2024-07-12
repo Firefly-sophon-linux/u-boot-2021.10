e25ada31dfac3d7e12d049c098b2c176ecc4ac7c
Levin Du <zslevin@gmail.com>
2024-07-12 09:44:07 +0800

    Support customized leds in also USB flashing
    Change-Id: If98623072d624306ae340fba130f720e86cc52ce
    Signed-off-by: Levin Du <zslevin@gmail.com>

--- cmd/cvi_update.c
+++ cmd/cvi_update.c
@@ -287,6 +287,33 @@ static int _usb_update(uint32_t usb_pid)
 	return 0;
 }
 
+static void env_default_except_led(void)
+{
+	char *env;
+	char led_error[32] = {0};
+	char led_status[32] = {0};
+	// save led env
+	env = env_get("LED_ERROR");
+	if (env != NULL) {
+		strncpy(led_error, env, sizeof(led_error)-1);
+		led_error[sizeof(led_error)-1] = '\0';
+	}
+	env = env_get("LED_STATUS");
+	if (env != NULL) {
+		strncpy(led_status, env, sizeof(led_status)-1);
+		led_error[sizeof(led_status)-1] = '\0';
+	}
+	// set env to default
+	run_command("env default -a", 0);
+	// restore led env
+	if (strlen(led_error) > 0) {
+		env_set("LED_ERROR", led_error);
+	}
+	if (strlen(led_status) > 0) {
+		env_set("LED_STATUS", led_status);
+	}
+}
+
 static int do_cvi_update(struct cmd_tbl *cmdtp, int flag, int argc,
 			 char *const argv[])
 {
@@ -296,29 +323,8 @@ static int do_cvi_update(struct cmd_tbl *cmdtp, int flag, int argc,
 	if (argc == 1) {
 		update_magic = readl((unsigned int *)BOOT_SOURCE_FLAG_ADDR);
 		if (update_magic == SD_UPDATE_MAGIC) {
-			char *env;
-			char led_error[16] = {0};
-			char led_status[16] = {0};
-			// save led env
-			env = env_get("LED_ERROR");
-			if (env != NULL) {
-				strncpy(led_error, env, sizeof(led_error)-1);
-				led_error[sizeof(led_error)-1] = '\0';
-			}
-			env = env_get("LED_STATUS");
-			if (env != NULL) {
-				strncpy(led_status, env, sizeof(led_status)-1);
-				led_error[sizeof(led_status)-1] = '\0';
-			}
-			// set env to default
-			run_command("env default -a", 0);
-			// restore led env
-			if (strlen(led_error) > 0) {
-				env_set("LED_ERROR", led_error);
-			}
-			if (strlen(led_status) > 0) {
-				env_set("LED_STATUS", led_status);
-			}
+			//run_command("env default -a", 0);
+			env_default_except_led();
 			#if defined(CONFIG_ROOTFS_UBUNTU) || defined(CONFIG_ROOTFS_DEBIAN)
 			ret = run_command("load ${devtype} ${devnum}:${distro_bootpart} ${scriptaddr} /$ota_path/boot.scr;source ${scriptaddr}", 0);
 			if (ret != 0) {
@@ -329,7 +335,8 @@ static int do_cvi_update(struct cmd_tbl *cmdtp, int flag, int argc,
 			ret = _storage_update(sd_dl);
 			#endif
 		} else if (update_magic == USB_UPDATE_MAGIC) {
-			run_command("env default -a", 0);
+			//run_command("env default -a", 0);
+			env_default_except_led();
 			usb_pid = 0;
 			ret = _usb_update(usb_pid);
 		}
